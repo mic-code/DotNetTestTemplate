626e6210119498c0a08bef4245781324bdd55749
Michael Yuen <michael@overimagined.com>
2025-05-13 12:31:08 +0800

    Delete rename script after exceute

--- RenameProject.ps1
+++ RenameProject.ps1
@@ -5,3 +5,5 @@ Rename-Item -Path "$TemplateName.sln" -NewName "$ProjectName.sln"
 Rename-Item -Path ".\$TemplateName\$TemplateName.csproj" -NewName "$ProjectName.csproj"
 Rename-Item -Path "$TemplateName" -NewName "$ProjectName"
 (Get-Content "$ProjectName.sln").Replace("$TemplateName", "$ProjectName") | Set-Content "$ProjectName.sln"
+
+Remove-Item -Path ".\RenameProject.ps1"
